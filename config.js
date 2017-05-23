e9e9bf5c6ecb16cf9d9ba7489496b554d527bb34
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-23 15:23:56 +0200

    skype bot

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports ={
-    APIAI_ACCESS_TOKEN:"5b440dd9729d4f599de38ef1c6f1a648",
-    APP_ID:"0949d2cf-5a65-43b0-898d-7fa4fd20ec42",
-    APP_SECRET:"ZLYampdzMTNHiyKnvzAfUFQ",
-    SERVER_URL:"https://skchatb.herokuapp.com/",
-    fichier:'Exemple.xlsx',
-}
