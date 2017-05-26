d516ed11950dd9c89d79c1fe78873c48b961df37
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-26 10:32:31 +0200

    skype bot

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports={
     APIAI_ACCESS_TOKEN:"5b440dd9729d4f599de38ef1c6f1a648",
     APIAI_LANG:"fr",
-    APP_ID:"0949d2cf-5a65-43b0-898d-7fa4fd20ec42",
-    APP_SECRET:"ZLYampdzMTNHiyKnvzAfUFQ",
+    APP_ID:"84ee7a15-f9ff-477f-9ef7-496435b70d18",
+    APP_SECRET:"dDyoxh9pDoScyJt5fWcbNd5",
 }
