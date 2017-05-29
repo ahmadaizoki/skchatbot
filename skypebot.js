d05b6d786bade78ef07a053d40f3771ec9cb6a93
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-29 16:14:37 +0200

    skype bot

--- skypebot.js
+++ skypebot.js
@@ -104,7 +104,7 @@ module.exports = class SkypeBot {
                         if (err) throw err;
                         rows=client
                             .query(`SELECT id, projet, fonction, personne FROM projet;`)
-                        console.log(rows.toString());
+                        console.log(rows);
                         session.send(rows.toString());
                     });
 
