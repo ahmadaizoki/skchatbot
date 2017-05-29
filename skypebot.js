3a247e1fb157201b327de3d050df75556eb21c46
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-29 17:40:21 +0200

    skype bot

--- skypebot.js
+++ skypebot.js
@@ -100,35 +100,8 @@ module.exports = class SkypeBot {
                     let responses;
                     let text="";
                     let text1="";
-                    conn(req,res)
-                    {
-                        const results = [];
-                        // Get a Postgres client from the connection pool
-                        pg.connect(process.env.DATABASE_URL, (err, client, done) = > {
-                            // Handle connection errors
-                            if(err) {
-                                done();
-                                console.log(err);
-                                return res.status(500).json({success: false, data: err});
-                            }
-                            // SQL Query > Select Data
-                            const query = client.query('SELECT projet FROM projet;');
-                        // Stream results back one row at a time
-                        query.on('row', (row) = > {
-                            results.push(row);
-                    })
-                        ;
-                        // After all data is returned, close connection and return results
-                        query.on('end', () = > {
-                            done();
-                        console.log(results[0].projet);
-                        text1 = text + results[0].projet;
-                    })
-                        ;
-                        return res.send(text1);
-                    })
-                        ;
-                    }
+                    let text2=conn(text1);
+
 
 
                     if(intentName==="projet_fonction") {
@@ -207,7 +180,7 @@ module.exports = class SkypeBot {
                     }
 
                     if (SkypeBot.isDefined(responseMessages) && responseMessages.length > 0) {
-                        this.doRichContentResponse(session,text1);
+                        this.doRichContentResponse(session,text2);
                     } else if (SkypeBot.isDefined(responseText)) {
                         console.log(sender, 'Response as text message');
                         session.send(responseText);
@@ -244,5 +217,34 @@ module.exports = class SkypeBot {
         }
 
         return obj != null;
+    }
+    conn(text1)
+    {
+        const results = [];
+        // Get a Postgres client from the connection pool
+        pg.connect(process.env.DATABASE_URL, (err, client, done) = > {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                return res.status(500).json({success: false, data: err});
+            }
+            // SQL Query > Select Data
+            const query = client.query('SELECT projet FROM projet;');
+        // Stream results back one row at a time
+        query.on('row', (row) = > {
+            results.push(row);
+    })
+        ;
+        // After all data is returned, close connection and return results
+        query.on('end', () = > {
+            done();
+        console.log(results[0].projet);
+        text1 = text + results[0].projet;
+    })
+        ;
+        return res.send(text1);
+    })
+        ;
     }
 }
