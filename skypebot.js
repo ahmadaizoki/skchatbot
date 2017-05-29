bcfd924364efb64ac11ce7a2907b76a8af2a6643
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-29 17:49:12 +0200

    skype bot

--- skypebot.js
+++ skypebot.js
@@ -64,6 +64,33 @@ module.exports = class SkypeBot {
         });
 
     }
+    conn(text1)
+    {
+        const results = [];
+        // Get a Postgres client from the connection pool
+        pg.connect(process.env.DATABASE_URL, (err, client, done) = > {
+            // Handle connection errors
+            if(err) {
+                done();
+                console.log(err);
+                return res.status(500).json({success: false, data: err});
+            }
+            // SQL Query > Select Data
+            const query = client.query('SELECT projet FROM projet;');
+        // Stream results back one row at a time
+        query.on('row', (row) = > {
+            results.push(row);
+    });
+        // After all data is returned, close connection and return results
+        query.on('end', () = > {
+            done();
+    };
+        console.log(results[0].projet);
+        text1 = text + results[0].projet;
+        return text1;
+    })
+        ;
+    }
 
     processMessage(session) {
 
@@ -217,32 +244,5 @@ module.exports = class SkypeBot {
         }
 
         return obj != null;
-    }
-    conn(text1)
-    {
-        const results = [];
-        // Get a Postgres client from the connection pool
-        pg.connect(process.env.DATABASE_URL, (err, client, done) = > {
-            // Handle connection errors
-            if(err) {
-                done();
-                console.log(err);
-                return res.status(500).json({success: false, data: err});
-            }
-            // SQL Query > Select Data
-            const query = client.query('SELECT projet FROM projet;');
-        // Stream results back one row at a time
-            query.on('row', (row) = > {
-                results.push(row);
-            });
-        // After all data is returned, close connection and return results
-            query.on('end', () = > {
-                done();
-            };
-        console.log(results[0].projet);
-        text1 = text + results[0].projet;
-        return text1;
-    })
-        ;
     }
 }
