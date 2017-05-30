219836b482738993333dc25c5d737192bcc402f4
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-30 18:08:08 +0200

    skype bot

--- select.js
+++ select.js
@@ -2,10 +2,18 @@
 //const request = require('request');
 const config = require('./config');
 const pg = require('pg');
-pg.defaults.ssl = true;
+var promise = require('bluebird');
 
-module.exports = function(callback,fonctionID){
-            if (!fonctionID) {
+pg.defaults.ssl = true;
+var options = {
+    // Initialization Options
+    promiseLib: promise
+};
+var pgp = require('pg-promise')(options);
+
+
+module.exports = function(fonctionID){
+           /* if (!fonctionID) {
                 var per=fonctionID;
 
                 pg.connect(process.env.DATABASE_URL, function (err, client) {
@@ -19,8 +27,27 @@ module.exports = function(callback,fonctionID){
                         });
                     console.log("resulta "+results);
                 })
-                callback(per);
+                callback(select);
             }else {
                 console.error('error');
-            }
+            }*/
+    var db = pgp(process.env.DATABASE_URL);
+
+    function getPersonne(req, res, next) {
+        db.any('SELECT personne FROM projet WHERE fonction='${fonctionID}'')
+            .then(function (data) {
+                res.status(200)
+                    .json({
+                        status: 'success',
+                        data: data,
+                        message: 'Retrieved Personne'
+                    });
+            })
+            .catch(function (err) {
+                return next(err);
+            });
+    }
+
+
+
 }
