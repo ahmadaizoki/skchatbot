225006fba16ba71d8316b52e9fadb1b513e61cab
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-05-30 17:36:33 +0200

    skype bot

--- select.js
+++ select.js
@@ -16,6 +16,7 @@ module.exports = function(callback,fonctionID){
                             text1=results.push(row);
                             console.log(results);
                         });
+                    callback(fonctionID);
                     console.log("resulta "+results);
                 });
             }else {
