8707cf2173279cce762a44406e5fff181322a229
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-06-01 10:32:30 +0200

    skype bot

--- select.js
+++ select.js
@@ -13,8 +13,8 @@ module.exports = function(projetID,fonctionID){
     console.log(fonctionID)
     db.any(`SELECT personne FROM projet WHERE projet='${projetID}' AND fonction='${fonctionID}'`)
         .then(data => {
-            console.log(data);
-            return;
+            console.log(data.toString());
+            return data.toString();
         })
         .catch(error =>{
             console.log('ERROR1:', error);
