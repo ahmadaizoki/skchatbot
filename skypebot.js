574b0dac43873e4a8681dc1461fe170b1dd01d79
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-06-20 11:38:44 +0200

    Skype bot

--- skypebot.js
+++ skypebot.js
@@ -147,8 +147,7 @@ module.exports = class SkypeBot {
                                 fonction=fonction.toLowerCase();
                                 projet=projet.toLowerCase();
                                 console.log(fonction);
-                                db2.any(`SELECT fonction FROM raccourcis WHERE rec1='${fonction}'`)
-                                //db2.any(`SELECT fonction FROM raccourcis WHERE /*fonction='${fonction}' OR rec1='${fonction}' OR rec2='${fonction}' OR rec3='${fonction}' OR rec4='${fonction}' OR rec5='${fonction}'`)
+                                db2.any(`SELECT fonction FROM raccourcis WHERE /*fonction='${fonction}' OR rec1='${fonction}' OR rec2='${fonction}' OR rec3='${fonction}' OR rec4='${fonction}' OR rec5='${fonction}'`)
                                     .then(data2 =>{
                                       for (var j in data2){
                                         console.log(data2[j].fonction);
